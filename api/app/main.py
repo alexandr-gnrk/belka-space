5070a87ab07d7a0743fef93a84e049fc0455225d
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-09-05 02:29:43 +0300

    Registered tortoise in FastApi app.

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 
-app = FastAPI()
+app = FastAPI(title='Belka Space API')
 
 
 @app.get('/')
 async def root():
     return {"message": "Hello World!"}
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://hollow.db",
+    modules={"models": ['orm.models']},
+    generate_schemas=True,
+    add_exception_handlers=True,)
