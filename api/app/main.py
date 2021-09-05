fbf2428e98d9c3fac72e04875767caba107d15fa
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-09-05 12:03:19 +0300

    Project structure was changed.

--- api/app/main.py
+++ api/app/main.py
@@ -13,6 +13,6 @@ async def root():
 register_tortoise(
     app,
     db_url="sqlite://hollow.db",
-    modules={"models": ['orm.models']},
+    modules={"models": ['core.models.database']},
     generate_schemas=True,
     add_exception_handlers=True,)
