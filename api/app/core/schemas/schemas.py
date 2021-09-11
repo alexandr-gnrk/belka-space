9a13a7aad09a8753a64d6fa03ab97403e1e33fee
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-09-11 16:42:18 +0300

    Pydantic income models updated with exclude_readonly param.

--- api/app/core/schemas/schemas.py
+++ api/app/core/schemas/schemas.py
@@ -7,20 +7,23 @@ from ..models import models
 # initialise tortoise models relationships
 Tortoise.init_models(["app.core.models.models"], "models")
 
-ImageIn = pydantic_model_creator(models.Image, name='ImageIn', exclude='id')
+ImageIn = pydantic_model_creator(
+    models.Image, name='ImageIn', exclude_readonly=True)
 Image = pydantic_model_creator(models.Image, name='Image')
 
 EmploymentStatus = pydantic_model_creator(
-    models.EmploymentStatus, name='EmploymentStatusIn', exclude='id')
+    models.EmploymentStatus, name='EmploymentStatusIn', exclude_readonly=True)
 EmploymentStatus = pydantic_model_creator(
     models.EmploymentStatus, name='EmploymentStatus')
 
 FacultyIn = pydantic_model_creator(
-    models.Faculty, name='FacultyIn', exclude='id')
+    models.Faculty, name='FacultyIn', exclude_readonly=True)
 Faculty = pydantic_model_creator(models.Faculty, name='Faculty')
 
-UserIn = pydantic_model_creator(models.User, name='UserIn', exclude='id')
+UserIn = pydantic_model_creator(
+    models.User, name='UserIn', exclude_readonly=True)
 User = pydantic_model_creator(models.User, name='User')
 
-EventIn = pydantic_model_creator(models.Event, name='Event', exclude='id')
+EventIn = pydantic_model_creator(
+    models.Event, name='Event', exclude_readonly=True)
 Event = pydantic_model_creator(models.Event, name='Event')
