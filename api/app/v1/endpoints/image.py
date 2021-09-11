f17d6720836c609161d5f040d48f891178157718
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-09-11 18:30:00 +0300

    Added type hinting for delete method.

--- api/app/v1/endpoints/image.py
+++ api/app/v1/endpoints/image.py
@@ -3,6 +3,7 @@ from tortoise.exceptions import DoesNotExist
 
 from ...core.models.models import Image
 from ...core.schemas.schemas import ImageSchema, ImageInSchema
+from ..type_hints import DeletedObject
 
 
 router = APIRouter(
@@ -16,18 +17,18 @@ async def create(image: ImageInSchema):
     return await ImageSchema.from_tortoise_orm(new_image)
 
 
-@router.get('/{id_}', response_model=ImageSchema)
-async def retrive(id_: int):
-    return await ImageSchema.from_queryset_single(Image.get(id=id_))
+@router.get('/{obj_id}', response_model=ImageSchema)
+async def retrive(obj_id: int):
+    return await ImageSchema.from_queryset_single(Image.get(id=obj_id))
 
 
-@router.delete('/{id_}', response_model=dict)
-async def delete(id_: int):
+@router.delete('/{obj_id}', response_model=DeletedObject)
+async def delete(obj_id: int):
     try:
-        await Image.filter(id=id_).delete()
+        await Image.filter(id=obj_id).delete()
     except DoesNotExist:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Image {id_} not found.')
+            detail=f'Image {obj_id} not found.')
 
-    return {'deleted': id_}
+    return {'deleted': obj_id}
